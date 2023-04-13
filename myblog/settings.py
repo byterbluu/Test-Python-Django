3c3416bde9dda3fce3de0fa1019c950ac7d6493d
Jose <ynujose@gmail.com>
2023-04-13 13:50:56 -0400

    DJANGO TEST

--- myblog/settings.py
+++ myblog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cv@=kzoh23lg@hj049y%x@g!702&jh(yrkhwwvwkda0vr)z=3_'
+SECRET_KEY = 'YOURKEYHERE'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
